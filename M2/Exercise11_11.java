9c00ac7384b0b9ecbd3a0ae024dc7f2f6c1659a4
efoust11 <efoust11@ivytech.edu>
2026-01-24 11:21:23 -0500

    M2 assignment 2

--- M2/Exercise11_11.java
+++ M2/Exercise11_11.java
@@ -1,38 +1,34 @@
+import java.util.Scanner;
+
 public class Exercise11_11 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first side of the triangle: ");
+        double side1 = input.nextDouble();
+        System.out.print("Enter the second side of the triangle: ");
+        double side2 = input.nextDouble();
+        System.out.print("Enter the third side of the triangle: ");
+        double side3 = input.nextDouble();
+
+        System.out.print("Enter the color of the triangle: ");
+        String color = input.next();
+        System.out.print("Is the triangle filled in? True or False: ");
+        boolean filled = input.nextBoolean();
+
+        input.close();
+
+        Triangle newTriangle = new Triangle(side1,side2,side3);
+
+        newTriangle.setColor(color);
+        newTriangle.setFilled(filled);
+        
+        
+        System.out.println("The area of the triangle is:" + newTriangle.getArea());
+        System.out.println("The perimeter of the triangle is: "+ newTriangle.getPerimeter());
+        System.out.println("The color of the triangle is: "+ newTriangle.getColor());
+        System.out.println("Is the triangle filled?: " + newTriangle.isFilled());
+    }
     
 }
 
-abstract class Triangle extends GeometricObject{
-    //default data fields
-    double side1 = 1.0;
-    double side2 = 1.0;
-    double side3 = 1.0;
-
-    //no-arg constructor
-    Triangle(){}
-
-    //constructor with three sides
-    Triangle(double newSide1, double newSide2, double newSide3){
-        side1 = newSide1;
-        side2 = newSide2;
-        side3 = newSide3;
-    }
-
-    //accessor methods
-    double getSide1(){return side1;}
-    double getSide2(){return side2;}
-    double getSide3(){return side3;}
-
-    //getArea()
-    public double getArea(){
-        double s = (side1 + side2 + side3)/2;
-        double area = Math.pow(s*(s-side1)*(s-side2)*(s-side3),0.5);
-        return area;
-    }
-    
-    //toString()
-    public String toString() {
-        return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
-    }
-
-}
