ef32111ef4f47ff17350994db264028090c37293
efoust11 <efoust11@ivytech.edu>
2026-02-04 20:29:19 -0500

    M4 Assignment 1

--- M4/Exercise20_11/Exercise20_11.java
+++ M4/Exercise20_11/Exercise20_11.java
@@ -1,37 +1,80 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
-
+//Test file: C:\Users\ekfou\Desktop\JavaClass\SDEV200 Assignments\M4\Exercise20_11\Welcome20_11.java
 public class Exercise20_11 {
-    //for each character in the source code (read as a string from text file)
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        //String filename = input.nextLine();
+        System.out.print("Enter filename:");
+        String filename = input.nextLine();
 
-        //File file = new File(filename);
-        //Scanner content = new Scanner(file);
-        //String content = "test ( testing )"; // replace with content from an actual file later?
+        File file = new File(filename);
 
-    //check for (, {, or [. if it finds one, set that to a variable
-    //for(String c: content){
-    //    String opening = "";
-    //    if(c == "(" | c == "{" | c == "["){
-    //        opening = c;
-    //    }
-    //    if(opening == "("){
-    //        if(c == "]" | c == "}"){
-    //            System.out.print("Error, mis-matched pair");
-    //            break;
-    //        }
-    //        if(c == ")"){
-    //            opening = "";
-    //        }
-    //    }
-    //}
-    //continue checking the following characters.
-        //if the match is found, look for next opening
-        //if a non bracket character is found, keep looking for closing
-        //if a non match is found, break loop
-
+        if (file.exists()){
+            if(checkGrouping(file)){
+                System.out.println("Correct grouping pairs");
+            } else{
+                System.out.println("Incorrect grouping pairs");
+            }
+        } 
+    
         input.close();
     }
+
+    public static boolean checkGrouping(File file) throws Exception{
+        Scanner content = new Scanner(file);
+        String opening = "";
+        List<Character> arrayList = new ArrayList<>();
+
+        //convert file to array of characters
+        while(content.hasNext()){
+            char[] chararcters = content.next().toCharArray();
+            for(char c: chararcters){
+                arrayList.add(c);
+            }
+        }
+        for(char i: arrayList){
+            //convert each character to a string
+            String c = Character.toString(i);
+             //check for (, {, or [. if it finds one, set that to a variable        
+            if(c.equals("(") || c.equals("{") || c.equals("[")){
+                opening = c;
+            }
+        //if the match is found, look for next  (, {, or [
+        //if a non bracket character is found, keep looking for closing character
+        //if a non match is found, break loop
+            if(opening.equals("(")){
+                if(c.equals("]") || c.equals("}")){
+                    System.out.print("Error, mis-matched pair");
+                    content.close();
+                    return false;
+                }
+                if(c.equals(")")){
+                    opening = "";
+                }
+            }
+            if(opening.equals("[")){
+                if(c.equals(")") || c.equals("}")){
+                    System.out.print("Error, mis-matched pair");
+                    content.close();
+                    return false;
+                }
+                if(c.equals("]")){
+                    opening = "";
+                }
+            }
+            if(opening.equals("{")){
+                if(c.equals("]") || c.equals(")")){
+                    content.close();
+                    return false;
+                }
+                if(c.equals("}")){
+                    opening = "";
+                }
+            }
+        }
+        content.close();
+        return true;
+    }
 }
+    
+
---
+++ M4/Exercise20_11/Welcome20_11.java
@@ -0,0 +1,6 @@
+public class Welcome20_11 {
+    public static void main(String[] args) {
+        //display message Welcome to Java! on the console
+        System.out.println("Welcome to Java");
+    }
+}
